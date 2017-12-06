4e226489991a57cac9bdf6aaacf4b797b906c6ae
bekor <bekor88@gmail.com>
2017-12-07 00:39:05 +0100

    TLE gatherer through API with python package

---
+++ out/gatherSatTLE.py
@@ -0,0 +1,14 @@
+from spacetrack import SpaceTrackClient
+
+
+def main():
+	st = SpaceTrackClient('***REMOVED***', '***REMOVED***')
+	ids = [25544, 37820, 41765]
+	for i in ids:
+		lines = st.tle_latest(iter_lines=True, epoch='>now-30', limit=1, orderby='epoch', norad_cat_id=i, format='tle')
+		with open('satellites/' + str(i) + '.dat', 'w') as fp:
+			for line in lines:
+				fp.write(line + "\n")
+			
+if __name__ == "__main__":
+	main()
