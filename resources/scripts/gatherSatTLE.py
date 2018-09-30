7bb966c902df323b343ff6772ace0b740b25bd90
Marcell Bán <afga93+cc@gmail.com>
2018-09-30 14:31:28 +0200

    added fresh satellite data

--- resources/scripts/gatherSatTLE.py
+++ resources/scripts/gatherSatTLE.py
@@ -4,7 +4,7 @@ from spacetrack import SpaceTrackClient
 def main():
 	st = SpaceTrackClient('user', 'pw')
 	ids = [25544, 20580, 27540, 28485, 31135,\
-		  33053, 37820, 41765, 40889, 40890,\
+		  33053, 41765, 40889, 40890,\
 		  41175, 41174, 41550, 41549, 41859,\
 		  41860, 41861, 41862, 23118, 28378]
 	lines = st.tle_latest(iter_lines=True, epoch='>now-30', orderby='epoch', norad_cat_id=ids, format='tle')
