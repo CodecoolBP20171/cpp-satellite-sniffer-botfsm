9939dca396eb748804c14c6f05fc20053920be9a
Marcell Bán <afga93+cc@gmail.com>
2020-12-19 10:04:10 +0100

    build fixups

--- src/sniffer/types.h
+++ src/sniffer/types.h
@@ -8,6 +8,10 @@ struct texture_vertex {
   float posx, posy, texx, texy;
 };
 struct color_vertex {
+// ignore gcc maybe unused attribute warning
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattributes"
   [[maybe_unused]] float posx, posy, colr, colg, colb;
+#pragma GCC diagnostic pop
 };
 } // namespace Graphics
